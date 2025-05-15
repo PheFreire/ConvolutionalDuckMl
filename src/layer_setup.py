fb90459822cd33a25fd52ea16d22f0df8186b7d1
Pato <pheprogrammer@gmail.com>
2025-05-15 04:40:48 -0300

    Delete draft

--- src/layer_setup.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-from typing import Literal
-
-@dataclass
-class LayerSetup:
-    perceptrons_qnt: int
-    activation_function: Literal['relu', 'softmax', 'sigmoid']
